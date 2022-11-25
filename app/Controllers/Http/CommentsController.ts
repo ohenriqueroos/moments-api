6fbe1a3b535c1d4d0d3d2b59e69bfc3eb1217764
Henrique Roos <henriquemroos21@gmail.com>
2022-11-25 16:01:00 -0300

    first commit

---
+++ app/Controllers/Http/CommentsController.ts
@@ -0,0 +1,22 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Moment from 'App/Models/Moment'
+import Comment from 'App/Models/Comment'
+
+export default class CommentsController {
+  public async store({request, params, response}: HttpContextContract) {
+    const body = request.body()
+    const momentId = params.momentId
+
+    await Moment.findOrFail(momentId)
+
+    body.momentId = momentId
+
+    const comment = await Comment.create(body)
+    response.status(201)
+
+    return {
+      message: "Comentário adicionado com sucesso!",
+      data: comment,
+    }
+  }
+}
